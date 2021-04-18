bbefdfba9a8403165232fd7351923524b1ae8895
Bartosz Anioł <bartek.104@o2.pl>
2021-04-18 18:56:05 +0200

    Dodanie projektu

---
+++ lib/drawer.dart
@@ -0,0 +1,125 @@
+import "package:flutter/material.dart";
+import 'package:intl/intl.dart';
+
+class NoteDrawer extends StatefulWidget {
+  final Map<DateTime, List<dynamic>> eventy;
+  NoteDrawer(this.eventy);
+
+  List<Map<String, List<dynamic>>> get wydarzenia {
+    return List.generate(8, (index) {
+      final dzien = DateTime.now().add(Duration(days: index));
+      var dzionek = DateFormat('d-MM-y').format(dzien);
+      List<dynamic> lista = [];
+      eventy.forEach((key, value) {
+        if (key.day == dzien.day &&
+            key.weekday == dzien.weekday &&
+            key.month == dzien.month) {
+          lista = value;
+        }
+      });
+
+      return {dzionek: lista};
+    });
+  }
+
+  Widget halp(List<Map<String, List<dynamic>>> cosik) {
+    List<String> wartosci = [];
+    List<dynamic> coss = [];
+    cosik.forEach((element) {
+      element.forEach((key, value) {
+        if (value.isNotEmpty) {
+          wartosci.add(key);
+          coss.add(value);
+        }
+      });
+    });
+
+    if (wartosci.isNotEmpty) {
+      return ListView.builder(
+          scrollDirection: Axis.vertical,
+          shrinkWrap: true,
+          padding: const EdgeInsets.all(8),
+          itemCount: wartosci.length,
+          itemBuilder: (BuildContext context, int index) {
+            return Card(
+              color: Colors.blue[200],
+              elevation: 5,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'Dzien ${wartosci[index]}',
+                      style:
+                          TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: coss[index].length,
+                        itemBuilder: (BuildContext context, int indexx) {
+                          return Center(
+                            child: Text(
+                              '${coss[index][indexx]}',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          );
+                        }),
+                  ],
+                ),
+              ),
+            );
+          });
+    } else {
+      return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          "Nie ma zadnych zaplanowanych wydarzen",
+          style: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      );
+    }
+  }
+
+  @override
+  Note_DrawerState createState() => Note_DrawerState();
+}
+
+class Note_DrawerState extends State<NoteDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                color: Colors.blue[300],
+                height: 120,
+                width: double.infinity,
+                padding: EdgeInsets.only(top: 10, left: 5),
+                alignment: Alignment.centerLeft,
+                child: Center(
+                  child: Text(
+                    "Wydarzenia na obecny tydzien",
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ),
+              widget.halp(widget.wydarzenia),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,236 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:intl/intl.dart';
+
+import "drawer.dart";
+
+void main() {
+  runApp(MyApp());
+}
+
+Map<String, dynamic> encodeMap(Map<DateTime, dynamic> map) {
+  Map<String, dynamic> newMap = {};
+  map.forEach((key, value) {
+    newMap[key.toString()] = map[key];
+  });
+  return newMap;
+}
+
+Map<DateTime, dynamic> decodeMap(Map<String, dynamic> map) {
+  Map<DateTime, dynamic> newMap = {};
+  map.forEach((key, value) {
+    newMap[DateTime.parse(key)] = map[key];
+  });
+  return newMap;
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Kalendarz(),
+    );
+  }
+}
+
+class Kalendarz extends StatefulWidget {
+  @override
+  KalendarzState createState() => KalendarzState();
+}
+
+class KalendarzState extends State<Kalendarz> {
+  CalendarController _calendarController;
+  Map<DateTime, List<dynamic>> _events;
+  List<dynamic> _selectedEvents;
+  TextEditingController _textController;
+  SharedPreferences prefs;
+  @override
+  void initState() {
+    super.initState();
+    _calendarController = CalendarController();
+    _textController = TextEditingController();
+    _events = {};
+    _selectedEvents = [];
+    initializeDateFormatting('pl_PL', null);
+    initPrefs();
+  }
+
+  initPrefs() async {
+    prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _events = Map<DateTime, List<dynamic>>.from(
+          decodeMap(jsonDecode(prefs.getString("events") ?? "{}")));
+    });
+  }
+
+  void _openBottomSheet(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (bCtx) {
+        return SingleChildScrollView(
+          child: Container(
+            // color: Colors.blueAccent,
+            padding: EdgeInsets.all(10),
+            width: 200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextField(
+                  controller: _textController,
+                  decoration: InputDecoration(labelText: 'Tresc notatki'),
+                ),
+                SizedBox(
+                  height: 180,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          'Wybrana Data',
+                          style: TextStyle(
+                            fontSize: 17,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          DateFormat('d-MM-y')
+                              .format(_calendarController.selectedDay),
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                    FloatingActionButton(
+                      backgroundColor: Colors.blue,
+                      onPressed: () {
+                        if (_textController.text.isEmpty) return;
+                        setState(() {
+                          if (_events[_calendarController.selectedDay] !=
+                              null) {
+                            _events[_calendarController.selectedDay]
+                                .add(_textController.text);
+                          } else {
+                            _events[_calendarController.selectedDay] = [
+                              _textController.text
+                            ];
+                          }
+                        });
+                        prefs.setString(
+                            "events", jsonEncode(encodeMap(_events)));
+                        _textController.clear();
+                        Navigator.pop(context);
+                      },
+                      child: Icon(Icons.assignment_turned_in),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Kalendarz"),
+      ),
+      drawer: Theme(
+        data: Theme.of(context).copyWith(canvasColor: Colors.blue),
+        child: NoteDrawer(_events),
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/cos2.jpg"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: ListView(
+          children: [
+            TableCalendar(
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              onDaySelected: (date, events, holidays) {
+                setState(() {
+                  _selectedEvents = events;
+                });
+              },
+              headerStyle: HeaderStyle(
+                formatButtonVisible: false,
+                centerHeaderTitle: true,
+              ),
+              calendarStyle: CalendarStyle(
+                todayColor: Colors.red[700],
+                selectedColor: Theme.of(context).primaryColor,
+                markersColor: Theme.of(context).shadowColor,
+              ),
+              events: _events,
+              calendarController: _calendarController,
+              locale: 'pl_PL',
+              // events: _events,
+            ),
+            Container(
+              height: 320,
+              width: 400,
+              child: SingleChildScrollView(
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      ..._selectedEvents.map((event) => Card(
+                            color: Colors.blue[100],
+                            elevation: 10,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(left: 5),
+                                  child: Text(
+                                    event,
+                                    style: TextStyle(fontSize: 20),
+                                  ),
+                                ),
+                                IconButton(
+                                  icon: Icon(
+                                    Icons.delete_forever_rounded,
+                                    size: 25,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _events[_calendarController.selectedDay]
+                                          .remove(event);
+                                    });
+                                    prefs.setString("events",
+                                        jsonEncode(encodeMap(_events)));
+                                  },
+                                )
+                              ],
+                            ),
+                          ))
+                    ]),
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _openBottomSheet(context),
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
